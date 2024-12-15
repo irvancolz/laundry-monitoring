bd0b8610632b6c80c8db39fdfc5f79ab9c36c6b6
irvancolz <irvansaharudin@gmail.com>
2024-12-15 16:55:10 +0700

    crate laundry service table

---
+++ supabase/migrations/20241215094732_create_laundry_service_table.sql
@@ -0,0 +1,19 @@
+create type service_pricing_type as enum (
+'weight',
+'piece'
+);
+
+create table if not exists laundry_service (
+    "id" bigint primary key generated always as identity,
+    "name" text,
+    "pricing_type" service_pricing_type,
+    "code" text,
+    "service_time_hour" int,
+    "price" int,
+    "created_at" timestamptz,
+    "created_by" text,
+    "updated_at" timestamptz,
+    "updated_by" text,
+    "deleted_at" timestamptz,
+    "deleted_by" text
+)
